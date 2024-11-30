5638e520b21a311688f1be6182e0f65a8cef45fa
LitzyDiaz0 <2137000110@utna.edu.mx>
2024-11-30 17:19:52 -0600

    insercion de btn elegir imagen en agregar pan

--- lib/pages/agregarpan.dart
+++ lib/pages/agregarpan.dart
@@ -28,17 +28,9 @@ class AgregarPanPage extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            const Text(
-              'Agregar Pan',
-              style: TextStyle(
-                fontSize: 30,
-                fontFamily: 'Aleo',
-                color: Color.fromRGBO(96, 60, 30, 1),
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+            // Logo arriba del texto
             Padding(
-              padding: const EdgeInsets.all(16.0),
+              padding: const EdgeInsets.only(bottom: 16.0),
               child: ClipOval(
                 child: SizedBox(
                   width: 150,
@@ -50,59 +42,104 @@ class AgregarPanPage extends StatelessWidget {
                 ),
               ),
             ),
+            const Text(
+              'Agregar Pan',
+              style: TextStyle(
+                fontSize: 30,
+                fontFamily: 'Aleo',
+                color: Color.fromRGBO(96, 60, 30, 1),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton.icon(
+              onPressed: () {
+                // Aquí se implementará la lógica para abrir la galería/cámara
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(
+                    255, 240, 240, 240), // Fondo gris claro
+                foregroundColor: Colors.black, // Color del texto e ícono
+                side: const BorderSide(
+                  color: Colors.black, // Borde negro
+                  width: 1.5,
+                ),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              ),
+              icon: const Icon(Icons.image, size: 24), // Ícono de imagen
+              label: const Text(
+                'Elegir Imagen',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontFamily: 'Aleo',
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
             Expanded(
               child: SingleChildScrollView(
                 child: Container(
-                  padding: const EdgeInsets.all(
-                      26.0), // Padding interno del contenedor
+                  padding: const EdgeInsets.all(20.0),
                   decoration: BoxDecoration(
-                    color: const Color.fromARGB(
-                        255, 255, 255, 255), // Fondo blanco
-                    borderRadius: BorderRadius.circular(20), // Borde redondeado
+                    color: const Color.fromARGB(255, 255, 255, 255),
+                    borderRadius: BorderRadius.circular(20),
                     boxShadow: [
                       BoxShadow(
                         color: Colors.black.withOpacity(0.1),
                         spreadRadius: 2,
                         blurRadius: 4,
-                        offset: const Offset(0, 2), // Sombra del contenedor
+                        offset: const Offset(0, 2),
                       ),
                     ],
                   ),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      _buildTextField('Nombre del pan:'),
+                      _buildTextField('Nombre del pan:', fontSize: 14),
                       const SizedBox(height: 16),
-                      _buildTextField('Sabor:'),
+                      _buildTextField('Sabor:', fontSize: 10),
                       const SizedBox(height: 16),
-                      _buildTextField('Precio:',
-                          keyboardType: TextInputType.number),
-                      const SizedBox(height: 16),
-                      _buildTextField('Stock:',
-                          keyboardType: TextInputType.number),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: _buildTextField(
+                              'Precio:',
+                              keyboardType: TextInputType.number,
+                              fontSize: 14,
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                          Expanded(
+                            child: _buildTextField(
+                              'Stock:',
+                              keyboardType: TextInputType.number,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ],
+                      ),
                       const SizedBox(height: 32),
                       Center(
-                        // Centrado del botón
                         child: ElevatedButton(
                           onPressed: () {
                             // Acción de agregar pan
                           },
                           style: ElevatedButton.styleFrom(
-                            foregroundColor: Colors.black, // Color del texto
-                            backgroundColor: const Color.fromARGB(
-                                255, 255, 255, 255), // Fondo del botón
+                            foregroundColor: Colors.black,
+                            backgroundColor:
+                                const Color.fromARGB(255, 255, 255, 255),
                             padding: const EdgeInsets.symmetric(
-                                vertical: 10.0, horizontal: 50), // Relleno
+                                vertical: 10.0, horizontal: 50),
                             side: const BorderSide(
-                              color: Colors.black, // Color del borde
-                              width: 2, // Grosor del borde
+                              color: Colors.black,
+                              width: 2,
                             ),
                           ),
                           child: const Text(
                             'Agregar Pan',
                             style: TextStyle(
-                                fontSize: 18,
-                                fontFamily: 'Amethysta'), // Estilo del texto
+                                fontSize: 18, fontFamily: 'Amethysta'),
                           ),
                         ),
                       ),
@@ -117,33 +154,33 @@ class AgregarPanPage extends StatelessWidget {
     );
   }
 
-  Widget _buildTextField(String label,
-      {TextInputType keyboardType = TextInputType.text}) {
+  Widget _buildTextField(
+    String label, {
+    TextInputType keyboardType = TextInputType.text,
+    double fontSize = 16,
+  }) {
     return Container(
-      margin: const EdgeInsets.symmetric(
-          vertical: 10.0), // Separación entre los campos
-      padding: const EdgeInsets.symmetric(
-          horizontal: 12.0), // Relleno horizontal para los campos
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      padding: const EdgeInsets.symmetric(horizontal: 12.0),
       decoration: BoxDecoration(
-        color: Colors.white, // Fondo blanco del contenedor del campo
-        borderRadius: BorderRadius.circular(8), // Bordes redondeados
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
         boxShadow: [
           BoxShadow(
             color: Colors.black.withOpacity(0.1),
             spreadRadius: 2,
             blurRadius: 4,
-            offset: const Offset(0, 2), // Sombra del contenedor
+            offset: const Offset(0, 2),
           ),
         ],
       ),
       child: TextField(
         keyboardType: keyboardType,
-        style: const TextStyle(fontSize: 10), // Tamaño del texto ajustado
+        style: TextStyle(fontSize: fontSize),
         decoration: InputDecoration(
           labelText: label,
-          border: InputBorder.none, // Eliminar borde interno
-          contentPadding: const EdgeInsets.symmetric(
-              vertical: 8), // Ajuste de padding para que no estén tan grandes
+          border: InputBorder.none,
+          contentPadding: const EdgeInsets.symmetric(vertical: 8),
         ),
       ),
     );
